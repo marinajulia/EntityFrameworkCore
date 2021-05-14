779ab3434df5e4af0a49e9efc575d136f559cfac
Marina Julia <marinajulia707@gmail.com>
2021-05-14 17:23:34 -0300

    Criando um banco de dados

---
+++ CursoEFCore_Aula1/Migrations/20210514140702_ProdutoInicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoEFCore_Aula1.Migrations
+{
+    public partial class ProdutoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Preco = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ProdutoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ CursoEFCore_Aula1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CursoEFCore_Aula1 {
+
+    //entidade
+    class Produto {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+    //classe de contexto
+
+    class AppDbContext : DbContext {
+        //mapeamento da entidade para a tabela
+        public DbSet<Produto> Produtos{ get; set; } //a tabela vai ter o nome de "Produtos"
+
+        //O Provedor e a string de conexão
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-8024PRG;" +
+                " Initial Catalog=Aula1DB; Integrated Security = True");
+        }
+
+    }
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
